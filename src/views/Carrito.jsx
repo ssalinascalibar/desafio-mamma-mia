4c195ba530734e97f5272453a674d8176dc222d7
ssalinascalibar <ssalinascalibar@gmail.com>
2022-10-15 20:17:57 -0300

    mod style carrito component

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -19,11 +19,13 @@ export default function Carrito() {
             {carrito.map((c, i) => (
               <div className="row" key={i}>
                 <div className="carrito-content">
-                  <div className="col-md-6"> 
-                    <img src={c.img} alt={c.name} />
-                    {c.name}
+                  <div className="col-md-9">
+                    <div className="pizza-carrito"> 
+                      <img src={c.img} alt={c.name} />
+                      <h3>{c.name}</h3>
+                    </div>
                   </div>
-                  <div className="col-md-6">
+                  <div className="col-md-3">
                     <div className="botonera" key={i}>
                       <h5>${c.price}</h5>
                       <button type="button" className="btn btn-danger" onClick={() => restarPizza(i)}> - </button>
